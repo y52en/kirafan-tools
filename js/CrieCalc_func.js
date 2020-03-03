b839dc4ee304325f5b48fa3362e6f689f1b3ab31
y52en <7152yen@gmail.com>
2020-03-03 14:15:13 +0900

    ProjectPagesのテスト

---
+++ js/CrieCalc_func.js
@@ -0,0 +1,59 @@
+function CrieCalc(stm, awkn, N_awkn, poss5, poss4, memb5, memb4, stmFtype) {
+
+     // stmFtypeによってstmに代入する数字を決定
+     switch (stmFtype){
+       case "stamina":
+        stm = document.getElementById("stm_i").value; //入力欄の数字を代入
+        break;
+       case "Gkey":    
+        stm = 40;
+        break;
+       case "Skey":
+        stm = 30;
+        break;
+       case "Cquest":
+        stm = 100;
+        break;
+       case "tickets":
+        stm = 35
+        break;
+     }  ;
+
+    //不正チェック    
+    if (memb4 + memb5 > 5)
+    {result = "編成人数が不正です" ; return result};
+    if (awkn > 25)
+    {result = "総覚醒数が不正です" ; return result};
+    if (N_awkn > 5)
+    {result = "フル覚醒人数が不正です" ; return result};
+    if (memb4 > poss4 || memb5 > poss5)
+    {result = "所持人数より編成人数の方が多いです" ; return result};
+    if (N_awkn > memb5)
+    {result = "編成人数よりフル覚醒人数の方が多いです" ; return result};
+    if (awkn > memb4*4 + memb5*5 + (5 - memb4 - memb5)*3 )
+    {result = "総覚醒数、または編成人数が不正です" ; return result};
+    if ((stm || awkn || N_awkn || memb4 || memb5 || poss5 || poss4 )%1 != 0 || 
+        (stm || awkn || N_awkn || memb4 || memb5 || poss5 || poss4 )    < 0 )
+    {result = "小数、マイナスは入力できません" ; return result};
+    
+    //ファクター計算
+    //Math.froundを使いすぎだと思うが、どこまで必要なのか分からないのでこのまま
+    var stmF = Math.fround(Math.fround(stm) * 0.02);
+    var awknF = Math.fround(Math.fround(awkn) + 25 + Math.fround(N_awkn) * 10);
+    var possF = Math.fround(20 + Math.fround(memb5) * Math.fround(7 + Math.min(poss5,5)) 
+                        　　+ Math.fround(Math.fround(memb4) * Math.fround(1 + 0.4 * Math.fround( Math.min(poss4,5) ) )));
+    //クリエ計算
+    var result;
+    result = Math.fround(Math.fround(stmF) * Math.fround(awknF) * Math.fround(possF));
+
+    return result;
+};
+
+/* 
+計算式：https://twitter.com/UCieloSereno/status/1233659580309233666 
+
+stmF=stm*0.02   //スタミナファクター
+awknF=awkn+25+N_awkn*10     //覚醒ファクター
+possF=20+memb5*(7+poss5) + memb4*(1+0.4*poss4)      //所持ファクター
+
+*/
